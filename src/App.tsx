eed44da5dcf7f4a63964351a58253211f9c9a3b5
fyb-july <yibo.fang@dongmi.ai>
2021-02-25 22:53:16 +0800

    第一次提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './assets/logo.png';
+import styles from "./App.module.less"
+import Home from "./home/home"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+        <img src={logo} className={styles.fangLogo} alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+            欢迎来到我的主页
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Home/>
     </div>
   );
 }
-
 export default App;
---
+++ src/home/home.tsx
@@ -0,0 +1,10 @@
+import styles from "./home.module.less"
+
+const Home =()=>{
+    return (
+        <div className={styles.fangHome}>
+                hahah 
+        </div>
+    )
+}
+export default Home
